bbc6d5787008848eda46f5890aa0233f50b9e7a7
Milan Košťák <milankostak@seznam.cz>
2021-10-04 10:11:34 +0900

    Lecture 07 - Web Services - updated dependencies

--- Lecture07/build.gradle.kts
+++ Lecture07/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.1"
+    id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     war
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.spring") version "1.5.20"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
 group = "edu.kcg.web3"
@@ -16,8 +16,8 @@ repositories {
     mavenCentral()
 }
 
-val springBootVersion = "2.5.1"
-val kotlinVersion = "1.5.20"
+val springBootVersion = "2.5.5"
+val kotlinVersion = "1.5.31"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
@@ -28,8 +28,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.68") // password hashing
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.69") // password hashing
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
 }
