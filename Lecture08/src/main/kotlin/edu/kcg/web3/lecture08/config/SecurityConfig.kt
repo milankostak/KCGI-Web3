06c457588e5d9be8c6df90c9ce9afa13611b99f5
Milan Košťák <milankostak@seznam.cz>
2022-10-09 17:14:47 +0900

    Lecture 08 - ORM, Hibernate - replaced deprecated WebSecurityConfigurerAdapter

--- Lecture08/src/main/kotlin/edu/kcg/web3/lecture08/config/SecurityConfig.kt
+++ Lecture08/src/main/kotlin/edu/kcg/web3/lecture08/config/SecurityConfig.kt
@@ -4,18 +4,20 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig {
 
     private val logger = LoggerFactory.getLogger(SecurityConfig::class.java)
 
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         logger.info("Configuring Spring security")
         http.authorizeRequests().anyRequest().permitAll()
+        return http.build()
     }
 
     @Bean
@@ -24,7 +26,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 //        return BCryptPasswordEncoder(13)
 //        return SCryptPasswordEncoder()
         return Argon2PasswordEncoder()
-        // please do not use anything else (applies in 2021, WILL change in the future)
+        // please do not use anything else (applies in 2022, WILL change in the future)
     }
 
 }
