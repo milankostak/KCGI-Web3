f0f29fe92a2c64deec01e83356ad5e64937c47b7
Milan Košťák <milankostak@seznam.cz>
2022-10-09 16:33:08 +0900

    Lecture 06 - REST, GraphQL - updated dependencies

--- Lecture06/build.gradle.kts
+++ Lecture06/build.gradle.kts
@@ -1,23 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.6"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
     war
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
 }
 
 group = "edu.kcg.web3"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
-val springBootVersion = "2.5.6"
-val kotlinVersion = "1.5.31"
+val springBootVersion = "2.7.4"
+val kotlinVersion = "1.7.20"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
@@ -28,10 +28,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.69") // password hashing
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.72") // password hashing
 
-    implementation("com.graphql-java:graphql-java:17.3")
+    implementation("com.graphql-java:graphql-java:19.2")
     implementation("com.graphql-java:graphql-java-spring-boot-starter-webmvc:2.0")
     implementation("com.graphql-java-kickstart:graphiql-spring-boot-starter:11.1.0")
 
@@ -41,7 +41,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
