d20fbff4973d0bead3f92b63c411205d5bf3ee3c
Milan Košťák <milankostak@seznam.cz>
2022-10-17 19:19:34 +0900

    Lecture 03b - JSP, JSTL - updated to Jakarta 9

--- Lecture03b/src/main/kotlin/edu/kcg/web3/lecture03b/OtherServlet.kt
+++ Lecture03b/src/main/kotlin/edu/kcg/web3/lecture03b/OtherServlet.kt
@@ -1,9 +1,9 @@
 package edu.kcg.web3.lecture03b
 
-import javax.servlet.annotation.WebServlet
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 
 @WebServlet(value = ["/other"])
 class OtherServlet : HttpServlet() {
